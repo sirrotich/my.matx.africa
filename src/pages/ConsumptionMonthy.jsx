b694c1d621bdba90dde2b2c84094e4fcb42bd0b4
tkipngeno <tkipngeno@LTTECHMAC982.local>
2024-10-27 08:17:31 +0300

    updte profile

--- src/pages/ConsumptionMonthy.jsx
+++ src/pages/ConsumptionMonthy.jsx
@@ -119,7 +119,10 @@ const ConsumptionMonthly = () => {
   return (
     <div className="main-analytics-container">
       <div className="top-div">
-        <div className="top-header">{month} 2024</div>
+        <div className="top-header"><svg  onClick={() => navigate('/analytics')} width="30" height="37" viewBox="0 0 30 37" fill="none" xmlns="http://www.w3.org/2000/svg" >
+<path fill-rule="evenodd" clip-rule="evenodd" d="M7.81404 14.3501C8.08234 14.6184 8.08234 15.0534 7.81404 15.3217L5.32274 17.813H18.7786C19.1581 17.813 19.4657 18.1206 19.4657 18.5C19.4657 18.8794 19.1581 19.187 18.7786 19.187H5.32274L7.81404 21.6783C8.08234 21.9466 8.08234 22.3816 7.81404 22.6499C7.54574 22.9182 7.11075 22.9182 6.84245 22.6499L3.17832 18.9858C2.91003 18.7175 2.91003 18.2825 3.17832 18.0142L6.84245 14.3501C7.11075 14.0818 7.54574 14.0818 7.81404 14.3501Z" fill="#292927"/>
+</svg>
+{month} 2024</div>
         <button className='top-btn'><span className='top-btn-text'>Home Gas</span></button>
       </div>
       <div className="max-w">
