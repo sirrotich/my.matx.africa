a872090693388fd986aed306ab4a9076fadb00d8
sirrotich <47354971+sirrotich@users.noreply.github.com>
2025-01-15 15:40:13 +0300

    Add files via upload

---
+++ src/components/BarTooltip.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../styles/BarTooltip.css';
+
+const BarTooltip = ({ value, position, onViewDetails, type = 'gas', dataType }) => {
+  const getDisplayValue = () => {
+    switch (type) {
+      case 'gas':
+        return `${value.toFixed(2)} kg`;
+      case 'power':
+        return `${value} ${dataType === 'outage' ? 'Hrs Outage' : 'Hrs'}`;
+      case 'internet':
+        return `${value} ${dataType === 'offline' ? 'Hrs Offline' : 'GB'}`;
+      default:
+        return `${value.toFixed(2)}`;
+    }
+  };
+
+  return (
+    <div 
+      className="bar-tooltip"
+      style={{
+        left: position.x,
+        top: position.y
+      }}
+    >
+      <div className="tooltip-value">{getDisplayValue()}</div>
+      <button className="view-details-btn" onClick={onViewDetails}>
+        View Details
+      </button>
+    </div>
+  );
+};
+
+export default BarTooltip;
